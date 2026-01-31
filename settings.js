cee6b03f5746c36c045a87449add9290200c2766
Sarah <frejulian57@gmail.com>
2026-01-30 21:56:51 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
-global.owner = ['5218711426787']
+global.owner = ['559296077349']
 global.sessionName = 'Sessions/Owner'
 global.version = '^2.0'
 global.pairing_code = true
@@ -10,19 +10,19 @@ global.number_bot = ''
 global.api = {
   url: 'https://api.stellarwa.xyz',
   url2: 'https://sylphy.xyz',
-  key: 'proyectsV2', //saca ru apikey en la web stellarwa.xyz
-  key2: 'ApiKey' //saca tu apikey en la web sylphy.xyz
+  key: 'stellar-TlyXyQ1v', //saca ru apikey en la web stellarwa.xyz
+  key2: 'sylphy-56df94' //saca tu apikey en la web sylphy.xyz
 }
 
 global.bot = {
   api: 'https://api.stellarwa.xyz',
-  web: 'https://github.com/CheirZ'
+  web: 'alya.lat'
 }
 
 global.mods = ['5218711426787']
 
 global.msgglobal = '[Error: *TypeError*] fetch failed'
-globalThis.dev = '★彡[xɪ_ᴍɪɢᴜᴇʟᴏɴ77xx]彡★'
+globalThis.dev = 'ⁿᵏGwee ⚝'
 
 global.mess = {
   socket: '《✧》 Este comando solo puede ser ejecutado por un Socket.',
@@ -31,6 +31,6 @@ global.mess = {
 }
 
 global.my = {
-  ch: '120363405797868544@newsletter',
-  name: 'Hu Tao-Proyect (el respaldo)'
+  ch: '120363188537623366@newsletter',
+  name: 'yumi-Proyect (el respaldo)'
 }
