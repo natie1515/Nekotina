be8b4cd1db4eee114fc389bc5e5bf9156769262c
Sarah <frejulian57@gmail.com>
2026-02-03 22:25:03 -0400

    Update menu.js

--- commands/info/menu.js
+++ commands/info/menu.js
@@ -7,7 +7,7 @@ import moment from 'moment-timezone';
 const COMMANDS_URL = 'https://rest.alyabotpe.xyz/src/commands.js'
 
 export default {
-  command: ['allmenu', 'help', 'menu'],
+  command: ['allmenu', 'help', 'menu', 'Neko help'],
   category: 'info',
   run: async (client, m, args, command, text, prefix) => {
     try {
@@ -104,7 +104,7 @@ export default {
         })
       }
 
-menu += `\n> *${botname2} desarrollado por ZyxlJs* ૮(˶ᵔᵕᵔ˶)ა`
+menu += `\n> *${botname2} desarrollado por Gwee* ૮(˶ᵔᵕᵔ˶)ა`
 
 if (banner.endsWith('.mp4') || banner.endsWith('.gif') || banner.endsWith('.webm')) {
 await client.sendMessage(
